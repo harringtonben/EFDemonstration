d634d4eb5c875445ef39b0f801f0fe6eb75fee80
Ben Harrington <harringtonben@icloud.com>
2018-05-12 09:24:59 -0500

    finished project setup

---
+++ EFDemonstration/Models/Cow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EFDemonstration.Models
+{
+    public class Cow
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public decimal Weight { get; set; }
+        public CowType Type { get; set; }
+
+    }
+
+    public enum CowType
+    {
+        Dairy,
+        Wild,
+        Chocolate,
+        Sacrifical
+    }
+}
